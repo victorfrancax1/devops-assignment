0b9d0a156d4b51b4180a5bb4a5cd3f2198634fc5
Victor Ferreira <victor.ferreira@quintoandar.com.br>
2021-02-07 19:00:03 -0300

    removing start file

--- start.sh
+++
@@ -1,4 +0,0 @@
-kubectl create -f database/pgsql-storageclass.yaml
-kubectl create -f database/pgsql-storage-resources.yaml
-kubectl create -f database/pgsql-creds.yaml
-kubectl create -f database/pgsql-statefulset.yaml
